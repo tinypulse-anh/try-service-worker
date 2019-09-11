21bbd05d538cb4b1c057a35f2b6749207d10566b
Anh Tran <anhtran@tinypulse.com>
2019-09-11 14:08:02 +0700

    Auto cache local assets with SW

--- sw.js
+++ sw.js
@@ -6,21 +6,31 @@ var urlsToCache = [
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) return response;
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
 
-        return fetch(event.request);
-      })
+      return fetch(event.request).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
   );
 });
